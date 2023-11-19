f7146883b35072fcc371d9127b10bad8c7fe0b0e
Daniel Tinivella <dtinivella@gmail.com>
2023-11-19 12:19:09 -0300

    create basic configuration init.lua

---
+++ init.lua
@@ -0,0 +1,24 @@
+--
+-- init.lua
+-- Copyright (C) 2023 tinix <tinix@debian>
+--
+-- Distributed under terms of the MIT license.
+--
+
+-- load basic config
+require("settings")
+
+-- load plugins
+require("plugins")
+
+-- load keymaps
+require("keymaps")
+
+-- load themes
+require("themes")
+
+-- Config for LSP
+require("lsp")
+
+-- Config for Tree 
+require("nv-tree")
