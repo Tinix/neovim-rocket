ff1fa5f920c0530a3e7bd790cbd928b2759bb92a
Daniel Tinivella <dtinivella@gmail.com>
2023-11-27 16:34:17 -0300

    add Plug to plugins

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -28,7 +28,83 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-tree.lua'
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-lualine/lualine.nvim'
+  use 'neoclide/coc.nvim'
   
+
+  -- Themes
+  use 'EdenEast/nightfox.nvim'
+  use 'sainnhe/sonokai'
+  use 'sainnhe/gruvbox-material'
+  use 'sainnhe/edge'
+  use 'arcticicestudio/nord-vim'
+  use 'marko-cerovac/material.nvim'
+  use 'navarasu/onedark.nvim'
+  use 'folke/tokyonight.nvim'
+  use 'bluz71/vim-nightfly-guicolors'
+  use 'bluz71/vim-moonfly-colors'
+  use 'rebelot/kanagawa.nvim'
+  use 'catppuccin/nvim'
+  use 'fatinch/vim-monokai'
+  use 'patstockwell/vim-monokai-tasty'
+  use 'sainnhe/everforest'
+  use 'tomasiser/vim-code-dark'
+  use 'ayu-themes/ayu-vim'
+  use 'chriskempson/base16-vim'
+  use 'jpo/vim-railscasts-theme'
+  use 'jacoborus/tender.vim'
+  use 'rakr/vim-one'
+  use 'dracula/vim'
+  use 'vim-airline/vim-airline'
+  use 'vim-airline/vim-airline-themes'
+  use 'junegunn/fzf'
+  use 'junegunn/fzf.vim'
+  use 'airblade/vim-rooter'
+  use 'terryma/vim-multiple-cursors'
+  use 'machakann/vim-highlightedyank'
+  use 'prettier/vim-prettier'
+
+ --  Plug
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-endwise'
+  
+  use 'tpope/vim-dispatch'
+  use 'tpope/vim-projectionist'
+
+  -- Templates
+  use 'aperezdc/vim-template'
+  use 'junegunn/goyo.vim'
+  use 'junegunn/limelight.vim'
+  
+  -- Git
+  use 'airblade/vim-gitgutter'
+  use 'tpope/vim-fugitive'
+
+  -- Rust
+  use 'rust-lang/rust.vim'
+  use 'rhysd/vim-clang-format'
+  -- Ruby 
+  use 'tpope/vim-rails'
+  use 'vim-ruby/vim-ruby'
+  use 'tpope/vim-bundler'
+  use 'tpope/vim-rake'
+  use 'tpope/vim-rbenv'
+  use 'vim-ruby/vim-ruby'
+  use 'kana/vim-textobj-user'
+  use 'nelstrom/vim-textobj-rubyblock'
+  use 'kana/vim-textobj-entire'
+  use 'kana/vim-textobj-indent'
+  use 'kana/vim-textobj-line'
+  -- Go 
+  use 'fatih/vim-go'
+  use 'buoto/gotests-vim'
+  use 'mattn/vim-goimports'
+  use 'AndrewRadev/splitjoin.vim'
+  use 'ray-x/go.nvim'
+  use 'ray-x/guihua.lua'
+  use 'olexsmir/gopher.nvim'
+  use 'leoluz/nvim-dap-go'
 
   use 'nvim-treesitter/nvim-treesitter'
   use {
